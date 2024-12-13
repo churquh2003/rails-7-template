c7bbf4d13b36190fa9744faa690bdde8f2ac3d9c
Charlotte Urquhart <curquhart@uchicago.edu>
2024-12-13 06:04:41 +0000

    saveee

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,26 @@
 class EmailsController < ApplicationController
+  def show
+    @user = User.find(params[:user_id])
+    @email = @user.emails.find(params[:id])
+  end
+
+  def index
+    @emails = Email.all
+  end
+
+  def create
+    @email = Email.new(email_params)
+    if @email.save
+      redirect_to @email, notice: 'Email was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:subject, :body, :recipient, :user_id)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :emails
+  end
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
+  root "users#index"
 end
